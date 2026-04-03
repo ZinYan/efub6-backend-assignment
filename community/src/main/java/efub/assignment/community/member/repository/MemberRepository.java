98579412cb6ab48923f46fcbb261ebcf543c5599
Zin Wai Yan <zinwaiyan2226@gmail.com>
2026-04-03 12:49:41 +0900

    [feat] 멤버 repository 추가

---
+++ community/src/main/java/efub/assignment/community/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package efub.assignment.community.member.repository;
+
+import efub.assignment.community.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    // email duplicate test
+    Boolean existsByEmail(String email);
+}
